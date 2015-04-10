c14cdf1233e642e47f3770befc973dd7ea6bf794
Estu Mughni <eztu.coseenus@gmail.com>
2015-04-10 13:57:50 +0700

    Perubahan Database
    -Edit many to many relationship & one to many relationship
    -Edit inheritance

--- peminjaman/migrations/0001_initial.py
+++ peminjaman/migrations/0001_initial.py
@@ -11,39 +11,142 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='komentar',
+            name='Approval',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('pesan', models.CharField(max_length=140)),
+                ('approval', models.BooleanField()),
             ],
         ),
         migrations.CreateModel(
-            name='peminjaman',
+            name='Komentar',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pesan', models.CharField(max_length=140)),
+                ('timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Peminjaman',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tanggal', models.DateField()),
+                ('waktu_mulai', models.TimeField()),
+                ('waktu_selesai', models.TimeField()),
                 ('email', models.CharField(max_length=50)),
                 ('no_telp', models.CharField(max_length=13)),
                 ('nama_kegiatan', models.CharField(max_length=50)),
                 ('deskripsi', models.CharField(max_length=350)),
+                ('jumlah_peserta', models.IntegerField(default=0)),
+                ('approval_manager_ruangan', models.BooleanField()),
             ],
         ),
         migrations.CreateModel(
-            name='ruangan',
+            name='Pesan',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('pesan', models.CharField(max_length=140)),
+                ('timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Ruangan',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('gedung', models.CharField(max_length=1)),
                 ('lantai', models.CharField(max_length=1)),
                 ('nomor', models.CharField(max_length=5)),
+                ('kapasitas', models.IntegerField(default=0)),
             ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('username', models.CharField(max_length=20, serialize=False, primary_key=True)),
+                ('password', models.CharField(max_length=30)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Peminjam',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='peminjaman.User')),
+                ('nama', models.CharField(max_length=30)),
+            ],
+            bases=('peminjaman.user',),
+        ),
+        migrations.CreateModel(
+            name='Pengawas',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='peminjaman.User')),
+                ('nama', models.CharField(max_length=30)),
+                ('role', models.CharField(max_length=20)),
+                ('nip', models.CharField(default=0, max_length=10)),
+            ],
+            bases=('peminjaman.user',),
+        ),
+        migrations.AddField(
+            model_name='user',
+            name='pesan',
+            field=models.ManyToManyField(to='peminjaman.User', through='peminjaman.Pesan'),
+        ),
+        migrations.AddField(
+            model_name='pesan',
+            name='penerima_username',
+            field=models.ForeignKey(related_name='pengirim_username', to='peminjaman.User'),
+        ),
+        migrations.AddField(
+            model_name='pesan',
+            name='pengirim_username',
+            field=models.ForeignKey(related_name='penerima_username', to='peminjaman.User'),
         ),
         migrations.AddField(
             model_name='peminjaman',
             name='ruangan_id',
-            field=models.ForeignKey(to='peminjaman.ruangan'),
+            field=models.ForeignKey(to='peminjaman.Ruangan'),
         ),
         migrations.AddField(
             model_name='komentar',
             name='izin_id',
-            field=models.ForeignKey(to='peminjaman.peminjaman'),
+            field=models.ForeignKey(to='peminjaman.Peminjaman'),
+        ),
+        migrations.AddField(
+            model_name='komentar',
+            name='user_komentar',
+            field=models.ForeignKey(to='peminjaman.User'),
+        ),
+        migrations.AddField(
+            model_name='approval',
+            name='izin_id',
+            field=models.ForeignKey(to='peminjaman.Peminjaman'),
+        ),
+        migrations.CreateModel(
+            name='Mahasiswa',
+            fields=[
+                ('peminjam_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='peminjaman.Peminjam')),
+                ('npm', models.CharField(default=0, max_length=10)),
+            ],
+            bases=('peminjaman.peminjam',),
+        ),
+        migrations.CreateModel(
+            name='Manager_Ruangan',
+            fields=[
+                ('peminjam_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='peminjaman.Peminjam')),
+                ('nip', models.CharField(default=0, max_length=10)),
+            ],
+            bases=('peminjaman.peminjam',),
+        ),
+        migrations.AddField(
+            model_name='peminjaman',
+            name='approval_pengawas',
+            field=models.ManyToManyField(to='peminjaman.Pengawas', through='peminjaman.Approval'),
+        ),
+        migrations.AddField(
+            model_name='peminjaman',
+            name='peminjam_username',
+            field=models.ForeignKey(to='peminjaman.Peminjam'),
+        ),
+        migrations.AddField(
+            model_name='approval',
+            name='pengawas_username',
+            field=models.ForeignKey(to='peminjaman.Pengawas'),
         ),
     ]
--- peminjaman/models.py
+++ peminjaman/models.py
@@ -1,43 +1,33 @@
 from django.db import models
 
-# Create your models here.
-
-class ruangan(models.Model):
+class Ruangan(models.Model):
     gedung = models.CharField(max_length=1)
     lantai = models.CharField(max_length=1)
     nomor = models.CharField(max_length=5)
-    kapasitas = models.IntegerField()
+    kapasitas = models.IntegerField(default=0)
 
-class user(models.Model):
+class User(models.Model):
     username = models.CharField(max_length=20,primary_key=True)
     password = models.CharField(max_length=30)
+    pesan = models.ManyToManyField('self', through='Pesan', symmetrical=False)
+
+class Peminjam(User):
     nama = models.CharField(max_length=30)
 
-class peminjam(models.Model):
-    username = models.ForeignKey(user,primary_key=True)
-
-class pengawas(models.Model):
-    username = models.ForeignKey(user,primary_key=True)
-    password = models.CharField(max_length=30)
+class Pengawas(User):
     nama = models.CharField(max_length=30)
     role = models.CharField(max_length=20)
     nip = models.CharField(max_length=10,default=0)
 
-class manager_ruangan(models.Model):
-    username = models.ForeignKey(peminjam,primary_key=True)
-    password = models.CharField(max_length=30)
-    nama = models.CharField(max_length=30)
+class Manager_Ruangan(Peminjam):
     nip = models.CharField(max_length=10,default=0)
 
-class mahasiswa(models.Model):
-    username = models.ForeignKey(peminjam,primary_key=True)
-    password = models.CharField(max_length=30)
-    nama = models.CharField(max_length=30)
+class Mahasiswa(Peminjam):
     npm = models.CharField(max_length=10,default=0)
 
-class peminjaman(models.Model):
-    ruangan_id = models.ForeignKey(ruangan)
-    peminjam_username = models.ForeignKey(peminjam)
+class Peminjaman(models.Model):
+    ruangan_id = models.ForeignKey(Ruangan)
+    peminjam_username = models.ForeignKey(Peminjam)
     tanggal = models.DateField()
     waktu_mulai = models.TimeField()
     waktu_selesai = models.TimeField()
@@ -47,21 +37,21 @@ class peminjaman(models.Model):
     deskripsi = models.CharField(max_length=350)
     jumlah_peserta = models.IntegerField(default=0)
     approval_manager_ruangan = models.BooleanField()
+    approval_pengawas = models.ManyToManyField(Pengawas,through='Approval')
 
-class komentar(models.Model):
-    izin_id = models.ForeignKey(peminjaman)
+class Komentar(models.Model):
+    izin_id = models.ForeignKey(Peminjaman)
     pesan = models.CharField(max_length=140)
     timestamp = models.DateTimeField()
-    user_komentar = models.ForeignKey(user)
+    user_komentar = models.ForeignKey(User)
 
-class pesan(models.Model):
-    pengirim_username = models.ForeignKey(user, related_name='penerima_username')
-    penerima_username = models.ForeignKey(user, related_name='pengirim_username')
+class Pesan(models.Model):
+    pengirim_username = models.ForeignKey(User, related_name='penerima_username')
+    penerima_username = models.ForeignKey(User, related_name='pengirim_username')
     pesan = models.CharField(max_length=140)
     timestamp = models.DateTimeField()
 
-
-class approval_pengawas(models.Model):
-    pengawas_username = models.ForeignKey(pengawas)
-    izin_id = models.ForeignKey(peminjaman)
+class Approval(models.Model):
+    pengawas_username = models.ForeignKey(Pengawas)
+    izin_id = models.ForeignKey(Peminjaman)
     approval = models.BooleanField()
