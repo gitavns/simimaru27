6c39dcb6bdef1fd6cb0353d44e60504f863c1f11
Estu Mughni <eztu.coseenus@gmail.com>
2015-04-10 14:17:51 +0700

    Perubahan Model
    -Delete attribute password on class user

--- peminjaman/models.py
+++ peminjaman/models.py
@@ -8,7 +8,6 @@ class Ruangan(models.Model):
 
 class User(models.Model):
     username = models.CharField(max_length=20,primary_key=True)
-    password = models.CharField(max_length=30)
     pesan = models.ManyToManyField('self', through='Pesan', symmetrical=False)
 
 class Peminjam(User):
